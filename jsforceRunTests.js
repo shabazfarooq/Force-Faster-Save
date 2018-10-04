4609ebb34e8d955cf1757a0207883abf8b27fecd
Shabaz Farooq <shabazf@amazon.com>
2018-10-04 09:39:57 -0400

    changed let reference to var because phantomjs was failing. minor tweaks to run tests

--- jsforceRunTests.js
+++ jsforceRunTests.js
@@ -28,15 +28,18 @@ function jsforceRunTests(loginUrl, username, password, classCoverageName, testCl
       // Query Test Coverage
       return conn.tooling.query(`
         SELECT Coverage, NumLinesCovered, NumLinesUncovered
-        FROM ApexCodeCoverage
+        FROM ApexCodeCoverageAggregate
         WHERE ApexClassorTriggerId = '${apexClassOrTrigger_result.Id}'
+        ORDER BY CreatedDate DESC
+        LIMIT 1
       `);
         // AND ApexTestClassId = '01pF00000049LfZIAU'
     })
     .then((result) => {
       let apexCodeCoverage_result = result.records[0];
       let apexClassOrTriggerBody = apexClassOrTrigger_result.Body.split("\n");
-      let coveredAverage = (apexCodeCoverage_result.NumLinesCovered / apexCodeCoverage_result.NumLinesUncovered) * 100;
+      let totalLines = apexCodeCoverage_result.NumLinesCovered + apexCodeCoverage_result.NumLinesUncovered;
+      let coveredAverage = ((apexCodeCoverage_result.NumLinesCovered / totalLines) * 100).toFixed(2);
 
       // Display code lines
       console.log('\n\n');
@@ -56,11 +59,11 @@ function jsforceRunTests(loginUrl, username, password, classCoverageName, testCl
 
       // Display code coverage
       logger.logColor(
-        '\n'
+        '\n      '
+        + coveredAverage +  '%   |   '
         + apexCodeCoverage_result.NumLinesCovered
         + '/' 
-        + apexCodeCoverage_result.NumLinesUncovered 
-        + ' (' + coveredAverage +  ' %)',
+        + totalLines,
         'fgCyan'
       );
     })
--- logger.js
+++ logger.js
@@ -1,6 +1,6 @@
 var prefix = '    ';
 
-let colors = {
+var colors = {
   reset: "\x1b[0m",
   bright: "\x1b[1m",
   dim: "\x1b[2m",
@@ -8,7 +8,6 @@ let colors = {
   blink: "\x1b[5m",
   reverse: "\x1b[7m",
   hidden: "\x1b[8m",
-
   fgBlack: "\x1b[30m",
   fgRed: "\x1b[31m",
   fgGreen: "\x1b[32m",
@@ -17,7 +16,6 @@ let colors = {
   fgMagenta: "\x1b[35m",
   fgCyan: "\x1b[36m",
   fgWhite: "\x1b[37m",
-
   bgBlack: "\x1b[40m",
   bgRed: "\x1b[41m",
   bgGreen: "\x1b[42m",
@@ -25,17 +23,12 @@ let colors = {
   bgBlue: "\x1b[44m",
   bgMagenta: "\x1b[45m",
   bgCyan: "\x1b[46m",
-  bgWhite: "\x1b[47m",
-}
+  bgWhite: "\x1b[47m"
+};
 
 var logger = {
   log: function(message){
     console.log(prefix + message);
-  },
-  logColor: function(message, color){
-    let colorCode = colors[color] ? colors[color] : colors.reset;
-
-    console.log(colorCode + message + colors.reset);
   },
   logSaveStart: function(fileName, startTime){
     // console.log('Saving "' + fileName + '" @ ' + startTime);
@@ -54,6 +47,11 @@ var logger = {
     console.log(prefix + '* ');
     console.log(prefix + '* ' + errorMessage);
     console.log(prefix + '* ');
+  },
+  logColor: function(message, color){
+    var colorCode = colors[color] ? colors[color] : colors.reset;
+
+    console.log(colorCode + message + colors.reset);
   }
 }
 
