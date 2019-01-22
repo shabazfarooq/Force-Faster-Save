12e907ccf357099f78bb1f9b65eac96a917510a5
Shabaz Farooq <shabazf@amazon.com>
2019-01-21 21:45:03 -0500

    Execute anonymous now returns log

--- jsforceExecuteAnon.js
+++ jsforceExecuteAnon.js
@@ -2,6 +2,9 @@ var jsforce = require('jsforce');
 var fs = require("fs");
 var logger = require('./logger');
 
+// TODO: Clean up message prints
+// TODO: Check to see if additional filter querys are required for the apex log query. ie: type=API, userId=thisUser
+
 function jsforceExecuteAnon(loginUrl, username, password, saveFileFullPath, saveFileName){
   var executeAnonFileContents = readFile(saveFileFullPath);
 
@@ -11,7 +14,36 @@ function jsforceExecuteAnon(loginUrl, username, password, saveFileFullPath, save
     .then((result) => {
       logger.log('Login Successful');
 
+      // Execute Anonymously
       return conn.tooling.executeAnonymous(executeAnonFileContents);
+    })
+    .then((result) => {
+      // Determine if failure
+      if (!result || !result.compiled || !result.success) {
+        var err = "ERROR: Line " + result.line + ", Column " + result.column + " -- " + result.compileProblem;
+
+        if (result.exceptionStackTrace) {
+          err +="\nExceptionStackTrace: " + result.exceptionStackTrace;
+        }
+        if (result.exceptionMessage) {
+          err +="\nExceptionMessage: " + result.exceptionMessage;
+        }
+        
+        throw err;
+      }
+
+      // Query latest log file
+      return conn.query(`SELECT Id, 
+        LogUserId, LogLength, LastModifiedDate, Request, Operation, Application, Status, DurationMilliseconds, SystemModstamp, StartTime, Location
+        FROM ApexLog ORDER BY StartTime LIMIT 1`
+      );
+    })
+    .then((result) => {
+      if (!result || !result.records || !result.records[0].Id) {
+        throw "Error: no debug record exists";
+      }
+
+      return conn.tooling.getDebugLog(result.records[0].Id);
     })
     .then((result) => {
       console.log(result);
@@ -35,41 +67,5 @@ function readFile(saveFileFullPath){
   return soqlFileContents;
 }
 
-function displayResponse(response) {
-  
-}
-
 
 module.exports = jsforceExecuteAnon;
-
-
-
-
-
-
-
-
-
-
-
-// SUCCESS
-// {
-//   line: -1,
-//     column: -1,
-//       compiled: true,
-//         success: true,
-//           compileProblem: null,
-//             exceptionStackTrace: null,
-//               exceptionMessage: null
-// }
-
-// FAIL
-// {
-//   line: 1,
-//     column: 13,
-//       compiled: false,
-//         success: false,
-//           compileProblem: 'Unexpected token \'(\'.',
-//             exceptionStackTrace: null,
-//               exceptionMessage: null
-// }
